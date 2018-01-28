180701a3c49013e4864949e56e1ddeee420b9321
Alex Morega <alex@grep.ro>
2018-01-28 18:27:28 +0200

    patch the `file` source tree to fix jpeg bug
    references:
    * https://lists.pld-linux.org/mailman/pipermail/pld-cvs-commit/Week-of-Mon-20150216/416336.html
    * https://bugs.launchpad.net/ubuntu/+source/file/+bug/1717991

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@
 
 git clone https://github.com/file/file
 cd file
+
+curl 'http://git.pld-linux.org/gitweb.cgi?p=packages/file.git;a=blob_plain;f=name-use-count.patch;h=ef8561eee670839a2f8caca35ec7f3d04df91179;hb=HEAD' | patch -p1
+
 autoreconf -f -i
 ./configure --disable-silent-rules --prefix="`pwd`"
 make -j4
